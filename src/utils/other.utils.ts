3c55ca85a7ee5edae8f8f287e94538cf950c2097
Diego Hernández Herrera <diegohh0411@gmail.com>
2025-05-02 08:20:29 -0600

    refactor(utils): simplify permittedParams function and remove ObjectMap interface

--- src/utils/other.utils.ts
+++ src/utils/other.utils.ts
@@ -1,18 +1,21 @@
-interface ObjectMap {
-  [key: string]: any;
-}
+export const permittedParams = <T>(obj: object, properties: string[]): T => {
+  console.log({ obj })
 
-export const permittedParams = <T>(obj: ObjectMap, properties: string[]): T => {
   const permitted = Object.entries(obj)
     .filter(([key]) => properties.includes(key))
-    .reduce((acc: ObjectMap, [key, value]) => {
+    .reduce((acc: any, [key, value]) => {
       acc[key] = value;
       return acc;
-    }, {} as ObjectMap);
+    }, {});
 
-  const missingProperties: string[] = properties.filter(
-    property => !Object.prototype.hasOwnProperty.call(permitted, property)
-  );
+  console.log({ permitted })
+
+  const missingProperties: string[] = properties.filter(property => {
+    return !permitted[property];
+  });
+
+  console.log({ missingProperties })
+
   if (missingProperties.length > 0) {
     throw new Error(`Missing required properties: ${missingProperties.join(', ')}`);
   }
